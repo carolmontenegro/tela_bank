0058af83158a3e49876058a871e6e3e7371859d4
carol montenegro <40433303+carolmontenegro@users.noreply.github.com>
2022-03-05 11:50:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { CardConta, Container, Content, Header } from './src/components'
+
+
+
+export default function App(props) {
+
+	return (
+		<Container>
+			<Header name='Carol Montenegro' {...props} />
+			<Content {...props}>
+				<CardConta amount={1250.65} />
+			</Content>
+		</Container>
+	)
+}
